540abaaaffeb6ed3c4cfdee3b6c14f39ad8d24e3
ARJUN <arjunsharman06@gmail.com>
2022-09-07 12:14:51 -0400

    added the server folder

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// This will work
+app.use(express.urlencoded({ extended: true }));
+// for using the client folder in server.js
+app.use(express.static('../client'));
+app.use(express.json);
+
+// This will not work
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+// for using the client folder in server.js
+app.use(express.static('../client'));
+
+// Getting the HTML route in server.js
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function () {
+  console.log(`Listening on the PORT : ${PORT}`);
+});
